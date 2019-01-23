d8e7965b8102b324e183e9e984381c7cbb8cd2c1
rshrc <rishieric91@gmail.com>
2019-01-24 05:16:31 +0530

    Using normal python scripting apporach

---
+++ install_android_sdk.bash
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+if [ "${BASH_SOURCE[0]}" -ef "$0" ]
+then
+    echo "This script relies on information from your environment and thus should be sourced."
+    exit 1
+fi
+
+# Script to setup the Android SDK on a Linux System
+CUR_DIR="$(cd "$( dirname "$( readlink -f "${BASH_SOURCE[0]}" )" )" && pwd)"
+CUR_DIR="${CUR_DIR/setup/}"
+TOOLS_URL="https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip"
+ZIP_NAME=$(printf '%s\n' "${TOOLS_URL##*/}")
+mkdir -p ~/Android/Sdk/; cd ~/Android/Sdk || exit 1
+if axel -a -n 10 "${TOOLS_URL}"; then
+    unzip -o "${ZIP_NAME}"
+else
+    exit 1
+fi
+# Create repositories.cfg if not present
+if [ ! -f ~/.android/repositories.cfg ] ; then
+    touch ~/.android/repositories.cfg
+fi
+rm "${ZIP_NAME}"
+printf '\nexport ANDROID_HOME=~/Android/Sdk' >> ~/.bashrc
+source ~/.bashrc
+yes | "${ANDROID_HOME}"/tools/bin/sdkmanager --licenses
+
+source "${CUR_DIR}"/setup/setup_android_sdk_packages.bash "${CUR_DIR}/setup"
+cd "${CUR_DIR}"
---
+++ workstation.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import subprocess
+
+print("Work Station Ready to be Created!")
+
+print("Installing Git, TMUX, ZSHRC")
+os.system("sudo apt install git")
+os.system("sudo apt install tmux")
+os.system("sh -c \"$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)")
+
+# Installing most commanly used Python Libraries
+print("Installing required libraries for Machine Learning")
+os.system("sudo pip3 install -r machine_learning_req.txt")
+
+print("Installing required libraries for Web Dev (Django)")
+os.system("sudo pip3 install -r web_dev_req.txt")
+
+print("Installing requited node.js and it's dependencies")
+os.system("sudo apt install nodejs")
+os.system("sudo apt install npm")
+os.system("sudo apt-get install -y build-essential")
+os.system("npm install -g @angular/cli")
+os.system("sudo npm install -g create-react-app")
+
+print("Installing Android SDK")
+os.system("./install_android_sdk.bash")
+
+
+
