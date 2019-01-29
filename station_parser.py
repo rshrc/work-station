e3dfc9d96b3eebe843eafeecbe9e852c8732fbf4
rshrc <rishieric91@gmail.com>
2019-01-29 19:18:41 +0530

    Added all the arguments

---
+++ station_parser.py
@@ -0,0 +1,15 @@
+import argparse
+
+
+def get_station_parser():
+    station_parser = argparse.ArgumentParser()
+    station_parser.add_argument('--gzt', type=bool, default=False, help="Install Git, tmux and Oh-My-Zsh")
+    station_parser.add_argument('--pylib', type=bool, default=False, help="Install commonly used Python Libraries")
+    station_parser.add_argument('--dj', type=bool, default=False, help="Instal Django and it's common dependencies")
+    station_parser.add_argument('--mlibs', type=bool, default=False,
+                                help="Install mostly used Machine Learning Libraries")
+    station_parser.add_argument('--fend', type=bool, default=False, help="Install Node, React and Angular")
+    station_parser.add_argument('--android', type=bool, default=False, help="Install Android SDK")
+    args = station_parser.parse_args()
+
+    return args
--- workstation.py
+++ workstation.py
@@ -1,30 +1,37 @@
 import os
-import sys
-import subprocess
+import station_parser
+
+args = station_parser.get_station_parser()
 
 print("Work Station Ready to be Created!")
 
-print("Installing Git, TMUX, ZSHRC")
-os.system("sudo apt install git")
-os.system("sudo apt install tmux")
-os.system("sh -c \"$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)")
+if args.gzt:
+    print("Installing Git, tmux, Oh-My-Zsh")
+    os.system("sudo apt install git")
+    os.system("sudo apt install tmux")
+    os.system("sudo apt install zsh")
+    os.system("sh -c \"$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)")
+    os.system("sudo apt-get install fonts-powerline")
 
-# Installing most commanly used Python Libraries
-print("Installing required libraries for Machine Learning")
-os.system("sudo pip3 install -r /../machine-learning/machine_learning_req.txt")
+if args.pylib:
+    print("Installing required libraries for Machine Learning")
+    os.system("sudo pip3 install -r /../machine-learning/machine_learning_req.txt")
 
-print("Installing required libraries for Web Dev (Django)")
-os.system("sudo pip3 install -r /../server/web_dev_req.txt")
+if args.dj:
+    print("Installing required libraries for Web Dev (Django)")
+    os.system("sudo pip3 install -r /../server/web_dev_req.txt")
 
-print("Installing requited node.js and it's dependencies")
-os.system("sudo apt install nodejs")
-os.system("sudo apt install npm")
-os.system("sudo apt-get install -y build-essential")
-os.system("npm install -g @angular/cli")
-os.system("sudo npm install -g create-react-app")
+if args.mlibs:
+    print("Installing requited node.js and it's dependencies")
+    os.system("sudo apt install nodejs")
+    os.system("sudo apt install npm")
+    os.system("sudo apt-get install -y build-essential")
+    os.system("npm install -g @angular/cli")
+    os.system("sudo npm install -g create-react-app")
 
-print("Installing Android SDK")
-os.system("./android/install_android_sdk.bash")
+if args.android:
+    print("Installing Android SDK")
+    os.system("./android/install_android_sdk.bash")
 
 
 
