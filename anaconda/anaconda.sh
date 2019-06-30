77f8ef62effa474fd7752046357e3349656d6a82
Sarthak Agarwal <sarthakagarwal152@gmail.com>
2019-06-30 20:02:43 +0530

    Delete anaconda.sh

--- anaconda/anaconda.sh
+++
@@ -1,6 +0,0 @@
-cd /tmp
-curl -O https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
-bash Anaconda3-2019.03-Linux-x86_64.sh 
-source ~/.bashrc
-conda update --all && conda upgrade --all
-
